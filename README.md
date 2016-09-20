5833914a6c87749a122f7fadb95b990099ce9379
Andrew McGregor <andy@txm.net>
2016-09-20 11:59:22 +0100

    tests

--- README.md
+++ README.md
@@ -8,6 +8,14 @@ Developed for use in React html input's to watch for changes and AJAX run a vali
 
 [![NPM](https://nodei.co/npm/ajax-input-validator.png)](https://www.npmjs.com/package/ajax-input-validator)
 
+`let IW = new InputWatch({time:2000, validate_func:validate_func});`
+
+`<input onKeyUp={IW.keyUp} onPaste={IW.paste} onBlur{IW.blur} />`
+
 ## License
 
 MIT, see [LICENSE.md](http://github.com/txm/ajax-input-validator/blob/master/LICENSE.md) for details.
+
+## HEALTH WARNING
+
+The big fail is that the plugin is dumb by design and doesn't know what validation function is being executed, therefore I don't yet have a test I can run.
--- test.js
+++ test.js
@@ -1,7 +1,44 @@
-var inputValidator = require('./')
-var test = require('tape')
+import React from 'react';
+import { expect } from 'chai';
+import { mount, shallow } from 'enzyme';
 
-test('Developed for use in React <input />\'s to watch for changes and AJAX run a validator', function(t) {
-  
-  t.end()
-})
+import { InputWatch } from './';
+
+
+function validate_func(){
+  return 'OK';
+}
+
+let IW = new InputWatch({time:5000, validate_func:validate_func});
+
+
+function keyPress() {
+  IW.keyDown();
+  IW.keyUp();
+}
+
+function pressKey() {
+  setTimeout(function(){
+    keyPress();
+  }, 2000);
+}
+
+
+describe('InputWatch', () => {
+
+  it('simulate keypress', () => {
+    pressKey();
+    //expect(Foo.prototype.componentDidMount.calledOnce).to.equal(true);
+  });
+
+});
+
+
+describe('InputWatch', () => {
+
+  it('simulate timeout', () => {
+    IW.blur();
+    //expect(Foo.prototype.componentDidMount.calledOnce).to.equal(true);
+  });
+
+});
